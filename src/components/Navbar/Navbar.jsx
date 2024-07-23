d9f0b6155ac3fedc6855a822431db9cc7fb27a19
MatheusLopesLemos <m.lopeslemos96@gmail.com>
2024-07-23 19:50:00 -0300

    Adicionado botão 'Order' no Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from 'react-icons/fa6';
 
 const Navbar = () => {
   return <div>
@@ -17,19 +19,25 @@ const Navbar = () => {
                         Shopsy
                     </a>
                 </div>
-                {/*search bar and order button*/}
-                <div>
-                    <div className='group'>
+                {/*search bar*/}
+                <div className='flex justify-between items-center gap-4 '>
+                    <div className='relative group hidden sm:block'>
                         <input 
                             type='text' 
                             placeholder='Search' 
                             className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 
-                            rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus-border:1 focus:border-primary
-                            
-                            ' 
-                            
+                            rounded-full border border-gray-300 px-2 py-1 
+                            focus:outline-none focus-border:1 focus:border-primary' 
                         />
+                        <IoMdSearch 
+                        className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3' />
                     </div>
+                    {/*order button*/}
+                    <button onClick={() => alert("Ordering not available yet") } className='bg-gradient-to-r from-primary to-secondary
+                    transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+                        <span className='group-hover:block hidden transition-all duration-200'>Order</span>
+                        <FaCartShopping className='text-xl text-white drop-shadow-sm cursor-pointer'/>
+                    </button>
                 </div>
             </div>
         </div>
