9625be8f0adc21830ccb2ce35f5ef21880c44122
MatheusLopesLemos <m.lopeslemos96@gmail.com>
2024-08-22 15:12:29 -0300

    Adicionadas animações

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -46,13 +46,13 @@ const Hero = () => {
 
 
   return (
-    <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] gb-gray-100 
+    <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 
     flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
         {/*Background Pattern*/}
         <div className='h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9'>
         </div>
       {/*Hero Selection*/}
-        <div className='container pb-8 sm:pb-0 '>
+        <div className='container pb-8 sm:pb-0'>
             <Slider {...settings}>
                 {imageList.map((data) => (
                     <div>
@@ -60,9 +60,20 @@ const Hero = () => {
                         {/*Text Content section*/}
                         <div className='flex flex-col justify-center gap-4 sm:pt-0 text-center sm:text-left 
                         order-2 sm:order-1 relative z-10 px-5'>
-                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold '>{data.title}</h1>
-                            <p className='text-sm'>{data.description}</p>
-                            <div>
+                            <h1 
+                            data-aos='zoom-in'
+                            data-aos-duration='500'
+                            data-aos-once='true'
+                            className='text-5xl sm:text-6xl lg:text-7xl font-bold '>{data.title}</h1>
+                            <p 
+                            data-aos='zoom-in'
+                            data-aos-duration='500'
+                            data-aos-once='true'
+                            className='text-sm'>{data.description}</p>
+                            <div
+                            data-aos='zoom-in'
+                            data-aos-duration='500'
+                            data-aos-once='true'>
                                 <button className='bg-gradient-to-r from-primary to-secondary hover:scale-105 
                                 duration-200  py-2 px-4 rounded-full '>
                                     Order Now
@@ -71,7 +82,11 @@ const Hero = () => {
                         </div>
                         {/*Image section*/}
                         <div className='order-1 sm:order-2'>
-                            <div className='relative z-10'>
+                            <div 
+                            data-aos='zoom-in'
+                            data-aos-once='true'
+                            className='relative z-10'>
+
                                 <img src={data.img} alt="" 
                                 className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px]
                                  sm:scale-125 lg:scale-125 object-contain mx-auto' />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -67,13 +67,17 @@ const Products = () => {
             <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5'>
                 {/*Card Section*/}
                 {ProductsData.map((data) => (
-                    <div key={data.id}>
+                    <div 
+                    data-aos="fade-up"
+                    data-aos-delay={data.aosDelay}
+                    key={data.id} 
+                    className='space-y-3'>
                         <img src={data.img} alt=''
                         className='h-[220px] w-[150px] object-cover rounded-md' />
                         <div>
-                        <h3 className='font-semibold'>{data.title}</h3>
-                        <p className='text-sm text-gray-600 '>{data.color}</p>
-                        <div className='flex items-center gap-1'>
+                        <h1 data-aos="fade-up" className='font-semibold'>{data.title}</h1>
+                        <p data-aos="fade-up" className='text-sm text-gray-600 '>{data.color}</p>
+                        <div data-aos="fade-up" className='flex items-center gap-1'>
                             <FaStar className='text-yellow-400'/>
                             <span>{data.rating}</span>
                         </div>
